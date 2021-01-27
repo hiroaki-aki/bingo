c6799a791b00fba9675f9903e8027aafe3e7ceff
hiroaki-aki <61532511+hiroaki-aki@users.noreply.github.com>
2021-01-27 21:48:50 +0900

    Update bingo.php

--- bingo.php
+++ bingo.php
@@ -10,9 +10,6 @@
 ・命名規則はPSRに従う（ただし配列の場合は何次元配列であるかをスネークケースで表示する）。
   例：2次元配列の場合：testNumber_2array
 ・配列の添字は全て「0」始まり。
-・アルゴリズムは問題文で指定されている通りの方法とする（短縮等はしない）。
-・コーディングは可読性を考慮しつつ実行処理速度が速いコーディングとする。
-  例：ループで使用する変数はループ外で宣言 or 初期値設定する等。
 --------------------------------------------------------------------------------------------
 */
 
@@ -24,12 +21,12 @@ while ($line = trim(fgets(STDIN)) ) {
 }
 
 /* ------ 設定値の取得（標準入力として決まっている値として取得） ------ */
-define('BINGO_CARD_CELL', (int)$inputLine_1array[0]);			// ビンゴカードの縦/横の「行数」
-define('BINGO_CARD_START', 1);						// ビンゴカードの単語が格納されている「最初の添字」
-define('BINGO_CARD_END', BINGO_CARD_CELL);				// ビンゴカードの単語が格納されている「最後の添字」
-define('WORD_NUMBER', (int)$inputLine_1array[BINGO_CARD_CELL + 1]);	// 選択された単語の「単語数」
-define('WORD_START', BINGO_CARD_CELL + 2);				// 選択された単語が格納された「最初の添字」
-define('WORD_END', BINGO_CARD_CELL + WORD_NUMBER + 1);			// 選択された単語が格納された「最後の添字」
+define('BINGO_CARD_CELL', (int)$inputLine_1array[0]);                // ビンゴカードの縦/横の「行数」
+define('BINGO_CARD_START', 1);                                       // ビンゴカードの単語が格納されている「最初の添字」
+define('BINGO_CARD_END', BINGO_CARD_CELL);                           // ビンゴカードの単語が格納されている「最後の添字」
+define('WORD_NUMBER', (int)$inputLine_1array[BINGO_CARD_CELL + 1]);  // 選択された単語の「単語数」
+define('WORD_START', BINGO_CARD_CELL + 2);                           // 選択された単語が格納された「最初の添字」
+define('WORD_END', BINGO_CARD_CELL + WORD_NUMBER + 1);               // 選択された単語が格納された「最後の添字」
 
 /* ------ 「ビンゴカード」の作成（3次元配列） ------ */
 // 三次元配列の構成は以下の通り。
@@ -68,11 +65,12 @@ foreach($bingoCard_3array as $row => $bingoCardLine_2array){
 
 /* ------ 「ビンゴカード」の内の「印」の有無を判定する ------ */
 
-$winWordDiagonallyRightDownNumber = 0;	
-$diagonallyRightDownRow           = 0;
-$diagonallyRightDownCol           = 0;
-$diagonallyLeftDownRow            = 0;
-$diagonallyLeftDownCol            = 0;
+$winWordDiagonallyRightDownNumber = 0;  // 左上→右下ラインの印の数
+$diagonallyRightDownRow           = 0;  // 左上→右下ラインを判定する為の行数
+$diagonallyRightDownCol           = 0;  // 左上→右下ラインを判定する為の列数
+$winWordDiagonallyLeftDownNumber  = 0;  // 右上→左下ラインの印の数
+$diagonallyLeftDownRow            = 0;  // 右上→左下ラインを判定する為の行数
+$diagonallyLeftDownCol            = 0;  // 右上→左下ラインを判定する為の列数
 $win = false;
 
 //「ビンゴカード」配列のデータ取得ループ
